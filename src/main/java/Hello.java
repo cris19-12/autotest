1803787c50f38dfcac59290e071f71d33a5e5f82
Krstina Nykhtilina <k.nuhtilina@premiumitsolution.com>
2019-08-01 00:23:34 +0300

    Добавление метода для вывода даты в Hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
+import java.util.Date;
 public class Hello {
     public static void main(String[] args){
         System.out.println("Привет мир!");
+
+        Date date = new Date();
+        System.out.println(date.toString());
     }
 }
