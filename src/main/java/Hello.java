7ace80fbcf1d9c15f2bd41e6c324b30fa1ca4a52
Krstina Nykhtilina <k.nuhtilina@premiumitsolution.com>
2019-07-31 23:26:55 +0300

    Добавление переменных в Hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Привет мир!");
+
+        int x = (int) (Math.random() * 3 + 4);
+        int y = (int) (Math.random() * 5 + 1);
+        int z = (int) (Math.random() * 12 + 1);
     }
 }
